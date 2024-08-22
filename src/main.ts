2116f9ad001d5e5c367dd4f62be6d23c63050294
Eduardo <eduardo.meneses@c2sglobal.com>
2024-08-22 08:18:02 -0300

    feat: add swagger dependency and configured Swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
@@ -9,6 +10,15 @@ async function bootstrap() {
     const envService = app.get(EnvService)
 
     const port = envService.get("PORT")
+
+    const config = new DocumentBuilder()
+        .setTitle('Chaxis api')
+        .setDescription('Api de busca de veículos ideais em sua região')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(port);
 }
